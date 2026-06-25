88e1d5dcfc3848bcf19ccef80640e13d7886aaf2
Shashwat Singh <shashawat15@gmail.com>
2026-06-25 20:46:57 +0530

    Add files via upload

---
+++ A_The_Ultimate_Square.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        cout << (n + 1) / 2 << '\n';
+    }
+}
